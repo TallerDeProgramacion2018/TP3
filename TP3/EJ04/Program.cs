5f0ca96b37729d3f027fbc8a4ee9d9d1eff48846
Gaston Milano <gastonmilano15@gmail.com>
2018-10-12 21:59:07 -0300

    Falta comentar

--- TP3/EJ04/Program.cs
+++ TP3/EJ04/Program.cs
@@ -9,7 +9,7 @@ namespace EJ04
     class Program
     {
         static ControladorFachada Controlador = new ControladorFachada();
-        
+
         static void VentanaPrincipal()
         {
             Console.Clear();
@@ -25,6 +25,12 @@ namespace EJ04
                         VentanaEncriptar();
                         break;
                     }
+
+                case ConsoleKey.D2:
+                    {
+                        VentanaDesencriptar();
+                        break;
+                    }
             }
         }
 
@@ -38,6 +44,8 @@ namespace EJ04
             Console.WriteLine("1 - Encriptador Cesar");
             Console.WriteLine("2 - Encriptador AES");
             Console.WriteLine("3 - Encriptador Null");
+            Console.WriteLine();
+            Console.WriteLine("0 - Volver a menu principal");
 
             var entrada = Console.ReadKey();
             switch (entrada.Key)
@@ -47,6 +55,7 @@ namespace EJ04
                         Console.Clear();
                         Console.WriteLine($"La palabra encriptada es: {Controlador.Encriptar(cadena, "Cesar")}");
                         Console.ReadKey();
+                        VentanaPrincipal();
                         break;
                     }
 
@@ -55,6 +64,7 @@ namespace EJ04
                         Console.Clear();
                         Console.WriteLine($"La palabra encriptada es: {Controlador.Encriptar(cadena, "AES")}");
                         Console.ReadKey();
+                        VentanaPrincipal();
                         break;
                     }
 
@@ -63,11 +73,64 @@ namespace EJ04
                         Console.Clear();
                         Console.WriteLine($"La palabra encriptada es: {Controlador.Encriptar(cadena, "Null")}");
                         Console.ReadKey();
+                        VentanaPrincipal();
                         break;
                     }
 
                 case ConsoleKey.D0:
                     {
+                        VentanaPrincipal();
+                        break;
+                    }
+            }
+        }
+
+        static void VentanaDesencriptar()
+        {
+            Console.Clear();
+            Console.Write("Ingrese la palabra que desea desencriptar: ");
+            string cadena = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Seleccione el encriptador que desea utilizar");
+            Console.WriteLine("1 - Encriptador Cesar");
+            Console.WriteLine("2 - Eencriptador AES");
+            Console.WriteLine("3 - Encriptador Null");
+            Console.WriteLine();
+            Console.WriteLine("0 - Volve a ventana principal");
+
+            var entrada = Console.ReadKey();
+            switch (entrada.Key)
+            {
+                case ConsoleKey.D1:
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"La palabra desencriptada es: {Controlador.Desencriptar(cadena, "Cesar")}");
+                        Console.ReadKey();
+                        VentanaPrincipal();
+                        break;
+                    }
+
+                case ConsoleKey.D2:
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"La palabra desencriptada es: {Controlador.Desencriptar(cadena, "AES")}");
+                        Console.ReadKey();
+                        VentanaPrincipal();
+                        break;
+                    }
+
+                case ConsoleKey.D3:
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"La palabra desencriptada es: {Controlador.Desencriptar(cadena, "Null")}");
+                        Console.ReadKey();
+                        VentanaPrincipal();
+                        break;
+                    }
+
+                case ConsoleKey.D0:
+                    {
+                        VentanaPrincipal();
                         break;
                     }
             }
